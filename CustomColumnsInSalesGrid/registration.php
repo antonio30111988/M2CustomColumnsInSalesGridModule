372621ebee6b15104a0c5f71043338ea62321f34
Antonio Lolić <antonio.lolic@typeqast.com>
2020-04-04 13:48:28 +0200

    Module initialization

---
+++ CustomColumnsInSalesGrid/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright: 2020 (c) SwiftOtter Studios
+ *
+ * @author Jesse Maxwell
+ * @copyright Swift Otter Studios, 2/6/20
+ * @package default
+ **/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Antonio88_CustomColumnsInSalesGrid',
+    __DIR__
+);
